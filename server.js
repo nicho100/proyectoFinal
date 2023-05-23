f1f6d57bd6c590551ca36a46cee9252c244d5108
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-23 19:55:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-const { puerto, urlMongo } = require("./config/enviorment")
-const express=require('express')
+//importaciones
+const { routerLogins } = require("./routes/logins");
+const { puerto, urlMongo,tiempo} = require("./config/enviorment");
+//configuracion express
+const express=require('express');
 const {createServer}= require('http')
+const expressSession=require("express-session")
 const app= express()
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 const server=createServer(app)
+//configuracion sesion y passport
+const mongoStore=require("connect-mongo")
+const passport=require("passport")
+app.use(expressSession({secret:"secret",resave:true,saveUninitialized:true}))
+app.use(passport.initialize())//se usa passport para controlar los logins de los usuariosS
+app.use(passport.session())
+
+//configuracion sockets
 const socketIo = require('socket.io')
-const { addProduct, getProducts } = require("./controller/product")
 const io =socketIo(server)
+
+
+app.use(expressSession({//se crea una cookie
+    store: mongoStore.create({mongoUrl:urlMongo}),
+    secret:"secreto",
+    resave: true,
+    saveUninitialized:true,
+    cookie:{maxAge:tiempo},
+    rolling:true,
+}))
+
+
 const producto={
     name:"ventilador",
     price:20,
@@ -17,13 +40,6 @@ const producto={
     category:"casa"
 }
 
-const productosz=async()=> {await addProduct(producto)
-const productos= await getProducts();
-console.log(productos)
-}
-
-
-productosz()
-
+app.use('/',routerLogins)
 
 server.listen(puerto,(req,res)=>(console.log("funciona"))) 
