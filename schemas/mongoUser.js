238ace61bff36071f21d3026a78c0a334c9e0812
nicho100 <63875333+nicho100@users.noreply.github.com>
2023-05-24 14:55:17 -0300

    Update mongoUser.js

--- schemas/mongoUser.js
+++ schemas/mongoUser.js
@@ -3,6 +3,8 @@ const mongoose= require("mongoose")
 const userSchema=new mongoose.Schema({//creo el esquema de usuarios para mongo
     email:{type:String, required: true},
     password:{type:String,required:true},
+    fullname:{type:String,required:true},
+    phone:{type:String,required:true},
 })
 
 module.exports=mongoose.model("user",userSchema)
